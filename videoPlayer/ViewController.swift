1dc205ec03f96ac283843a29e061aa1b3ecc9430
gouldk <gouldk4@gmail.com>
2019-08-29 11:22:23 -0400

    Initial Commit

---
+++ videoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  videoPlayer
+//
+//  Created by Kyle Gould on 8/29/19.
+//  Copyright © 2019 Kyle Gould. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
