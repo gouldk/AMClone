fe715c427a1baa9b27ed1576ac4d7f015af2e6fb
gouldk <gouldk4@gmail.com>
2019-08-30 12:38:47 -0400

    Skeleton season view

---
+++ videoPlayer/EpisodeCell.swift
@@ -0,0 +1,14 @@
+//
+//  EpisodeCell.swift
+//  videoPlayer
+//
+//  Created by Kyle Gould on 8/30/19.
+//  Copyright © 2019 Kyle Gould. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
--- videoPlayer/ViewController.swift
+++ videoPlayer/ViewController.swift
@@ -8,13 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    // parse out individual episodes into this array from endpoint
+    let episodes = ["test1", "test2", "3", "4", "5", "6", "7"]
+    private let itemsPerRow: CGFloat = 3
+    private let reuseIdentifier = "EpisodeCell"
+    private let sectionInsets = UIEdgeInsets(top: 50.0,
+                                             left: 20.0,
+                                             bottom: 50.0,
+                                             right: 20.0)
+    
+    let seasonEndpoint = URL(string: "http://amc-api-br.svc.ds.amcn.com/v2/public/feed/episodes?show=preacher&season=season-4&device=iOS&restricted=false&publish_state=public")
+    let episodeEndpoint = URL(string: "http://amc-api-br.svc.ds.amcn.com/v2/public/feed/video_episodes?show=preacher&season=season-4&episode=episode-01-masada")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return episodes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! EpisodeCell
+        
+        cell.label.text = episodes[indexPath.item]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+    }
 
+}
 
+extension ViewController : UICollectionViewDelegateFlowLayout {
+    //1
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //2
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    //3
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    // 4
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
 }
 
